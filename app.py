a53cfdfe6bdca3589a7a814e66fcc007bf891ba2
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:38:29 -0500

    changed the different type of inputs

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print('Hello World!')
 
-first_name_input = int(input('enter input 1 here: '))
-last_name_input = int(input('enter input 2 here: '))
+first_name_input = input('enter input 1 here: ')
+last_name_input = input('enter input 2 here: ')
 id_input = int(input('enter input 3 here: '))
-title_input = int(input('enter input 4 here: '))
-annual_salary = input1 + input2 + input3 + input4
+title_input = input('enter input 4 here: ')
+annual_salary = int(input('Annual Salary: '))
 
 print('salary is:', salary)
