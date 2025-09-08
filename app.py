f838272701cbe9e555093ee13c507e4835145fb4
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:30:05 -0500

    added 4 varibapes

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print('Hello World!')
+
+input1 = int(input('enter input 1 here: '))
+input2 = int(input('enter input 2 here: '))
+input3 = int(input('enter input 3 here: '))
+input4 = int(input('enter input 4 here: '))
+
+
+salary = 1000
