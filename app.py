147d78263ab33f7a18c4990d6d2b26f2c3d6a9c2
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:41:20 -0500

    added new print for all the variables

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ last_name_input = input('Last Name: ')
 id_input = int(input('Id: '))
 title_input = input('Title: ')
 annual_salary = int(input('Annual Salary: '))
+
+print(first_name_input, last_name_input, id_input, title_input, annual_salary)
