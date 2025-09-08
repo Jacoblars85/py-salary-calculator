de1f04849eff677b6ace4b15a39b5c4f2f219ce1
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:34:00 -0500

    formatted

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ input1 = int(input('enter input 1 here: '))
 input2 = int(input('enter input 2 here: '))
 input3 = int(input('enter input 3 here: '))
 input4 = int(input('enter input 4 here: '))
-
-
 salary = input1 + input2 + input3 + input4
 
 print('salary is:', salary)
