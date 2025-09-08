956b01009dc6356efdc3d40591fa8887506d337c
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:44:31 -0500

    added total monthly variable

--- app.py
+++ app.py
@@ -4,4 +4,6 @@ id_input = int(input('Id: '))
 title_input = input('Title: ')
 annual_salary = int(input('Annual Salary: '))
 
+total_monthly_salary = annual_salary / 12
+
 print(first_name_input, last_name_input, id_input, title_input, annual_salary)
