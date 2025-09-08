cbcb542a55f8c0751209a2822f07bcc21e591434
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:36:35 -0500

    changed input names

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print('Hello World!')
 
-input1 = int(input('enter input 1 here: '))
-input2 = int(input('enter input 2 here: '))
-input3 = int(input('enter input 3 here: '))
-input4 = int(input('enter input 4 here: '))
-salary = input1 + input2 + input3 + input4
+first_name_input = int(input('enter input 1 here: '))
+last_name_input = int(input('enter input 2 here: '))
+id_input = int(input('enter input 3 here: '))
+title_input = int(input('enter input 4 here: '))
+annual_salary = input1 + input2 + input3 + input4
 
 print('salary is:', salary)
