d7bf810220305c60cbfadad94eda5f63dd2a65cf
Jacob Larson <hockeybobber@comcast.net>
2025-09-08 18:39:26 -0500

    fixed whats in the input

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 print('Hello World!')
 
-first_name_input = input('enter input 1 here: ')
-last_name_input = input('enter input 2 here: ')
-id_input = int(input('enter input 3 here: '))
-title_input = input('enter input 4 here: ')
+first_name_input = input('First Name: ')
+last_name_input = input('Last Name: ')
+id_input = int(input('Id: '))
+title_input = input('Title: ')
 annual_salary = int(input('Annual Salary: '))
 
 print('salary is:', salary)
